ad1b3fb69c89d447677d1b0be2576755d687a459
Steven Chen <22718607+just-stv@users.noreply.github.com>
2024-12-23 18:32:24 -0800

    Undid temp fix

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
     minimumCacheTTL: 60,
   },
   experimental: {
-    optimizeCss: false,
+    optimizeCss: true,
     optimizePackageImports: ["framer-motion"],
   },
 };
