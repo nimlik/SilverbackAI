bc0086900e71e01e4af7dda40f23d87ccc59f9c2
just-stv <22718607+just-stv@users.noreply.github.com>
2025-01-14 12:03:01 -0800

    Revert "fix: bottom padding for homepage"
    This reverts commit edad39b4d8f4fade49646d72d535df7b1fa65ff0.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import AnimatedDefinition from "@/components/home";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-14">
+    <div className="flex items-center justify-center min-h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex flex-col items-center bg-sora-bg/50 backdrop-blur-md rounded-lg p-4">
         <AnimatedDefinition />
       </div>
