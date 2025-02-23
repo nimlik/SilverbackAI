638c0312b192003c27c13a6011867007329c14b8
nimlik <warriorking@Warriors-MacBook-Air.local>
2025-02-22 18:16:55 -0600

    Remove submodule artifacts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   compress: true,
   poweredByHeader: false,
   reactStrictMode: true,
+  output: "export", // Required for static exports
+  distDir: "build", // Changes output folder from .next to build
   images: {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
@@ -15,4 +17,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
